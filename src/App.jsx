a49bf1966adc5cd401253c98ead4c0f9c6f4417a
Saidakbar Makhmudkhujaev <smahmudxojaev@gmail.com>
2021-06-15 22:43:27 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      hello
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|j?g|svg|gif)?$/,
+                use: 'file-loader'
+            },
+            {
+                test: /\.less$/i,
+                use: [
+                    {
+                        loader: "style-loader",
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                    {
+                        loader: "less-loader"
+                    }
+                ]
+            },
+        ]
+    },
+  };
+  
