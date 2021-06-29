1752b5382302ba5a42c415cebfdfcec59dae6788
saidakbar123 <smahmudxojaev@gmail.com>
2021-06-29 13:50:29 +0500

    fixed erorrs adn confict

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import counterReducer from './Counter/counter.reducer';
+import counterReducer from './reducers/counterReducer';
 
 const rootReducer = combineReducers({
     counter: counterReducer,
