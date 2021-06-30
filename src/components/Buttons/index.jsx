6bd305a33ed565af0c6f7c926ec7899e79cd5476
Davron Sattorov <davron_sattorov_99@mail.ru>
2021-06-30 18:13:40 +0500

    Buttons updated

--- src/components/Buttons/index.jsx
+++ src/components/Buttons/index.jsx
@@ -1,88 +1,107 @@
-import { makeStyles } from '@material-ui/core/styles';
-import LinearProgress from '@material-ui/core/LinearProgress';
-import './index.scss'
+import { makeStyles } from "@material-ui/core/styles";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import "./index.scss";
+
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: '100%',
-    '& > * + *': {
+    width: "100%",
+    "& > * + *": {
       marginTop: theme.spacing(2),
     },
   },
 }));
 
+export default function Button(props) {
+  const classes = useStyles();
+  const {
+    children,
+    icon,
+    color = "primary",
+    loading = false,
+    shape = "filled",
+    borderWidth = 2,
+    position = "left",
+    size = "medium",
+    ...rest
+  } = props;
 
-export default function Button (props) {
-    const classes = useStyles();
-    const { 
-        children,
-        icon = '',
-        color= "text-gray-900",
-        loading = false,
-        shape = 'filled',
-        position = 'left',
-        size = 'medium',
-        ...rest 
-    } = props
-    console.log(children)
-    const getSize = (key) => {
-        switch (key) {
-            case 'small':
-                return {
-                    size: 'px-3 py-1',
-                    fontSize: 'text-xs'
-                }
-            case 'medium':
-                return {
-                    size: 'px-3 py-1 min:w-7 min:h-7',
-                    fontSize: 'text-sm'
-                }
-            case 'large':
-                return {
-                    size: 'px-6 py-2',
-                    fontSize: 'text-base'
-                }
-        
-            default:
-                break;
-        }
-    }
+  console.log(children);
 
-    const getShape = (key) => {
-        console.log(key)
-        switch (key) {
-            case 'filled':
-                return { color: color + ' iconColor-filled hover:bg-secondary' }
-            case 'outlined':
-                return { color: color + ' bg-white iconColor-outlined text-gray-900  border hover:bg-background_2' }
-            case 'text':
-                return { color: ' bg-transparent iconColor-text ' + color }
-        
-            default:
-                return { color: 'iconColor-filled'};
-        }
+  const getSize = (key) => {
+    switch (key) {
+      case "small":
+        return {
+          size: "px-3 py-1",
+          fontSize: "text-xs",
+        };
+      case "medium":
+        return {
+          size: "px-3 py-1 min:w-7 min:h-7",
+          fontSize: "text-sm",
+        };
+      case "large":
+        return {
+          size: "px-6 py-2",
+          fontSize: "text-base",
+        };
+
+      default:
+        break;
     }
-    return (
-        <div>
-            <div className="">
-                <button
-                    type="button"
-                    className={`
+  };
+
+  const getShape = (key) => {
+    console.log(key);
+    switch (key) {
+      case "filled":
+        return {
+          color: `bg-${color} iconColor-filled hover:bg-blue-400`,
+          background: "",
+        };
+      case "outlined":
+        return {
+          color: `bg-transparent text-${color} border-${borderWidth} border-${color} hover:bg-background_2`,
+        };
+      case "text":
+        return { color: `bg-transparent text-${color} ` };
+
+      default:
+        return { color: "iconColor-filled" };
+    }
+  };
+
+  return (
+    <div>
+      <div className="">
+        <button
+          type="button"
+          className={`
                         rounded-lg
                         focus:outline-none
                         transition
-                        ${ children ? '' : 'w-9 h-9' }
+                        ${children ? "" : "w-9 h-9"}
                         focus:ring focus:border-blue-300 
                         ${getSize(size).size}
-                        ${props.disabled ? 'bg-gray-200 cursor-not-allowed' : getShape(shape).color}
-                        text-white`
-                    } 
-                    { ...rest}>
-                        <div className={`flex justify-around items-center ${children ? 'space-x-2' : ''} font-medium font-semibold`}>
-                                <div>{icon}</div>
-                                <div className={getSize(size).fontSize}>{ children }</div>
-                        </div>
-                    </button>
-            </div>
-        </div>
-    )
+                        ${
+                          props.disabled
+                            ? "bg-gray-200 cursor-not-allowed"
+                            : getShape(shape).color
+                        }
+                        text-white 
+                        `}
+          {...rest}
+        >
+          <div
+            className={`flex justify-around items-center ${
+              children ? "space-x-2" : ""
+            } font-medium font-semibold`}
+          >
+            {position === "left" && icon}
+            <div className={getSize(size).fontSize}>{children}</div>
+            {position === "right" && icon}
+          </div>
+        </button>
+      </div>
+    </div>
+  );
 }
--- src/views/buttons/index.jsx
+++ src/views/buttons/index.jsx
@@ -1,70 +1,208 @@
-import Button from '../../components/Buttons/index'
-import Wrapper from '../../components/Template/index.jsx'
+import Button from "../../components/Buttons/index";
+import Wrapper from "../../components/Template/index.jsx";
+import FolderIcon from "@material-ui/icons/Folder";
 
-const folder = <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M4 8C4 7.46957 4.21071 6.96086 4.58579 6.58579C4.96086 6.21071 5.46957 6 6 6H11L13 8H18C18.5304 8 19.0391 8.21071 19.4142 8.58579C19.7893 8.96086 20 9.46957 20 10V16C20 16.5304 19.7893 17.0391 19.4142 17.4142C19.0391 17.7893 18.5304 18 18 18H6C5.46957 18 4.96086 17.7893 4.58579 17.4142C4.21071 17.0391 4 16.5304 4 16V8Z" />
-</svg>
+const folder = (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M4 8C4 7.46957 4.21071 6.96086 4.58579 6.58579C4.96086 6.21071 5.46957 6 6 6H11L13 8H18C18.5304 8 19.0391 8.21071 19.4142 8.58579C19.7893 8.96086 20 9.46957 20 10V16C20 16.5304 19.7893 17.0391 19.4142 17.4142C19.0391 17.7893 18.5304 18 18 18H6C5.46957 18 4.96086 17.7893 4.58579 17.4142C4.21071 17.0391 4 16.5304 4 16V8Z" />
+  </svg>
+);
 
-export default function App () {
-    return (
-        <Wrapper header="button list" className="font-body">
-            <h2>Filled</h2>
-            <div className="w-2/4 flex justify-start space-x-2 items-center">
-                <div className="mr-10">Small</div>
-                <div className=""><Button disabled onClick={e => alert()} icon={folder} size="small">Disabled</Button></div>
-                <div className=""><Button icon={folder} size="small">Default</Button></div>
-                <div className=""><Button size="small">Default</Button></div>
-            </div>
-            <div className="w-2/4 flex justify-start space-x-2 items-center mt-4">
-                <div className="mr-10">Medium</div>
-                <div className=""><Button icon={folder}>Default</Button></div>
-                <div className=""><Button icon={folder}>Default</Button></div>
-                <div className=""><Button >Default</Button></div>
-            </div>
-            <div className="w-2/4 flex justify-start space-x-2 items-center mt-4">
-                <div className="mr-10">Large</div>
-                <div className=""><Button icon={folder} size="large">Default</Button></div>
-                <div className=""><Button icon={folder} size="large">Default</Button></div>
-                <div className=""><Button size="large">Default</Button></div>
-            </div>
-            <hr className="my-10"></hr>
-            <h2>Outlined</h2>
-            <div className="w-2/4 flex justify-start space-x-2 items-center">
-                <div className="mr-10">Small</div>
-                <div className=""><Button disabled shape="outlined" onClick={e => alert()} icon={folder} size="small">Default</Button></div>
-                <div className=""><Button shape="outlined" icon={folder} size="small">Default</Button></div>
-                <div className=""><Button shape="outlined" size="small">Default</Button></div>
-            </div>
-            <div className="w-2/4 flex justify-start space-x-2 items-center mt-4">
-                <div className="mr-10">Medium</div>
-                <div className=""><Button shape="outlined" icon={folder}>Default</Button></div>
-                <div className=""><Button shape="outlined" icon={folder}>Default</Button></div>
-                <div className=""><Button shape="outlined" >Default</Button></div>
-            </div>
-            <div className="w-2/4 flex justify-start space-x-2 items-center mt-4">
-                <div className="mr-10">Large</div>
-                <div className=""><Button shape="outlined" icon={folder} size="large">Default</Button></div>
-                <div className=""><Button shape="outlined" icon={folder} size="large">Default</Button></div>
-                <div className=""><Button shape="outlined" size="large">Default</Button></div>
-            </div>
-            <hr className="my-10"></hr>
-            <h2>Text</h2>
-            <div className="w-2/4 flex justify-start space-x-2 items-center">
-                <div className="mr-10">Small</div>
-                <div className=""><Button disabled shape="text" onClick={e => alert()} icon={folder} size="small">Disabled</Button></div>
-                <div className=""><Button shape="text" icon={folder} size="small">Default</Button></div>
-            </div>
-            <div className="w-2/4 flex justify-start space-x-2 items-center mt-4">
-                <div className="mr-10">Medium</div>
-                <div className=""><Button shape="text" icon={folder}>Default</Button></div>
-                <div className=""><Button shape="text" icon={folder}>Default</Button></div>
-            </div>
-            <div className="w-2/4 flex justify-start space-x-2 items-center mt-4">
-                <div className="mr-10">Large</div>
-                <div className=""><Button shape="text" icon={folder} size="large">Default</Button></div>
-                <div className=""><Button shape="text" icon={folder} size="large">Large</Button></div>
-                <div className=""><Button shape="text" size="large">Large</Button></div>
-            </div>
-        </Wrapper>
-    )
+export default function App() {
+  return (
+    <Wrapper header="Button list" className="font-body">
+      <h2>Filled</h2>
+
+      <div className="w-2/4 flex justify-start space-x-2 items-center">
+        <div className="mr-10">Small</div>
+        <div className="">
+          <Button disabled onClick={(e) => alert()} icon={folder} size="small">
+            Disabled
+          </Button>
+        </div>
+        <div className="">
+          <Button
+            icon={<FolderIcon fontSize="small" />}
+            color="secondary"
+            size="small"
+          >
+            Default
+          </Button>
+        </div>
+        <div className="">
+          <Button size="small">Default</Button>
+        </div>
+      </div>
+      <div className="w-2/4 flex justify-start space-x-2 items-center mt-4">
+        <div className="mr-10">Medium</div>
+        <div className="">
+          <Button icon={<FolderIcon fontSize="medium" />}>Default</Button>
+        </div>
+        <div className="">
+          <Button icon={<FolderIcon fontSize="medium" />}>Default</Button>
+        </div>
+        <div className="">
+          <Button>Default</Button>
+        </div>
+      </div>
+      <div className="w-2/4 flex justify-start space-x-2 items-center mt-4">
+        <div className="mr-10">Large</div>
+        <div className="">
+          <Button icon={<FolderIcon fontSize="medium" />} size="large">
+            Default
+          </Button>
+        </div>
+        <div className="">
+          <Button icon={<FolderIcon fontSize="medium" />} size="large">
+            Default
+          </Button>
+        </div>
+        <div className="">
+          <Button size="large">Default</Button>
+        </div>
+      </div>
+      <hr className="my-10"></hr>
+      <h2>Outlined</h2>
+      <div className="w-2/4 flex justify-start space-x-2 items-center">
+        <div className="mr-10">Small</div>
+        <div className="">
+          <Button
+            disabled
+            shape="outlined"
+            onClick={(e) => alert()}
+            icon={<FolderIcon />}
+            size="small"
+          >
+            Default
+          </Button>
+        </div>
+        <div className="">
+          <Button
+            shape="outlined"
+            icon={<FolderIcon fontSize="small" />}
+            size="small"
+          >
+            Default
+          </Button>
+        </div>
+        <div className="">
+          <Button shape="outlined" size="small">
+            Default
+          </Button>
+        </div>
+      </div>
+      <div className="w-2/4 flex justify-start space-x-2 items-center mt-4">
+        <div className="mr-10">Medium</div>
+        <div className="">
+          <Button shape="outlined" icon={<FolderIcon fontSize="medium" />}>
+            Default
+          </Button>
+        </div>
+        <div className="">
+          <Button shape="outlined" icon={<FolderIcon fontSize="medium" />}>
+            Default
+          </Button>
+        </div>
+        <div className="">
+          <Button shape="outlined">Default</Button>
+        </div>
+      </div>
+      <div className="w-2/4 flex justify-start space-x-2 items-center mt-4">
+        <div className="mr-10">Large</div>
+        <div className="">
+          <Button
+            shape="outlined"
+            icon={<FolderIcon fontSize="medium" />}
+            size="large"
+          >
+            Default
+          </Button>
+        </div>
+        <div className="">
+          <Button
+            shape="outlined"
+            icon={<FolderIcon fontSize="medium" />}
+            size="large"
+          >
+            Default
+          </Button>
+        </div>
+        <div className="">
+          <Button shape="outlined" size="large">
+            Default
+          </Button>
+        </div>
+      </div>
+      <hr className="my-10"></hr>
+      <h2>Text</h2>
+      <div className="w-2/4 flex justify-start space-x-2 items-center">
+        <div className="mr-10">Small</div>
+        <div className="">
+          <Button
+            disabled
+            shape="text"
+            onClick={(e) => alert()}
+            icon={<FolderIcon fontSize="small" />}
+            size="small"
+          >
+            Disabled
+          </Button>
+        </div>
+        <div className="">
+          <Button
+            shape="text"
+            icon={<FolderIcon fontSize="small" />}
+            size="small"
+          >
+            Default
+          </Button>
+        </div>
+      </div>
+      <div className="w-2/4 flex justify-start space-x-2 items-center mt-4">
+        <div className="mr-10">Medium</div>
+        <div className="">
+          <Button shape="text" icon={<FolderIcon fontSize="medium" />}>
+            Default
+          </Button>
+        </div>
+        <div className="">
+          <Button shape="text" icon={<FolderIcon fontSize="medium" />}>
+            Default
+          </Button>
+        </div>
+      </div>
+      <div className="w-2/4 flex justify-start space-x-2 items-center mt-4">
+        <div className="mr-10">Large</div>
+        <div className="">
+          <Button
+            shape="text"
+            icon={<FolderIcon fontSize="medium" />}
+            size="large"
+          >
+            Default
+          </Button>
+        </div>
+        <div className="">
+          <Button
+            shape="text"
+            icon={<FolderIcon fontSize="medium" />}
+            size="large"
+          >
+            Large
+          </Button>
+        </div>
+        <div className="">
+          <Button shape="text" size="large">
+            Large
+          </Button>
+        </div>
+      </div>
+    </Wrapper>
+  );
 }
